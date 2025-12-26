f65fc383dca2febb51a5ad81c771ff3a86f0d3de
James Aworo <j.aworo@icloud.com>
2025-12-26 17:41:36 +0100

    Initial Commit

---
+++ SwitUIBasic/SwitUIBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitUIBasicApp.swift
+//  SwitUIBasic
+//
+//  Created by James Aworo on 12/26/25.
+//
+
+import SwiftUI
+
+@main
+struct SwitUIBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
